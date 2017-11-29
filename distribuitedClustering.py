d5314ba5819036ebe5037cd01eb75c38afd1a4a1
Your Name <you@example.com>
2017-11-29 14:49:51 -0200

    final fixes to script and changes to notebook

--- distribuitedClustering.py
+++ distribuitedClustering.py
@@ -149,7 +149,7 @@ def distribuited_fuzzy_C_means(data_batch, K, GPU_names, initial_centers, n_max_
 
     with tf.Session( config = config ) as sess:
         initialization_ts = time.time()
-        sess.run(tf.global_variables_initializer(), feed_dict={all_data: data_X})
+        sess.run(tf.global_variables_initializer(), feed_dict={all_data: data_batch})
         initialization_time = float( time.time() - initialization_ts ) 
     
         computation_time = 0.0
@@ -165,122 +165,6 @@ def distribuited_fuzzy_C_means(data_batch, K, GPU_names, initial_centers, n_max_
                     'computation_time'    : computation_time   ,
                     'n_iter'              : i+1
                 }
-    return end_resut
-
-def old_distribuited_k_means(X, K, GPU_names, initial_centers, n_max_iters):
-    setup_ts = time.time()
-    number_of_gpus = len(GPU_names)
-
-    X_list = np.split( X[ (X.shape[0] % number_of_gpus)  :, : ], number_of_gpus )
-    
-    partial_directions = []
-    partial_values = []
-    partial_results = []
-    #partial_cost = [] ## Commented for performance
-    
-    with tf.name_scope('global'):
-        with tf.device('/cpu:0'):
-            global_centroids = tf.Variable(initial_centers)
-            
-    for GPU_num in range(number_of_gpus):
-        GPU_name = GPU_names[GPU_num]
-        
-        (X_mat) = X_list.pop()
-        (N, M) = X_mat.shape
-  
-        with tf.name_scope('scope_' + str(GPU_num)):
-            with tf.device(GPU_name) :
-                ####
-                # In the coments we denote :
-                # => N = Number of Observations
-                # => M = Number of Dimensions
-                # => K = Number of Centers
-                ####
-                # Data for GPU GPU_num to Clusterize
-                X = tf.constant(X_mat)
-
-                # Reshapes rep_centroids and rep_points to format N x K x M so that 
-                # the 2 matrixes have the same size
-                rep_centroids = tf.reshape(tf.tile(global_centroids, [N, 1]), [N, K, M])
-                rep_points = tf.reshape(tf.tile(X, [1, K]), [N, K, M])
-
-                # Calculates sum_squares, a matrix of size N x K
-                # This matrix is not sqrt((X-Y)^2), it is just(X-Y)^2
-                # Since we need just the argmin(sqrt((X-Y)^2)) wich is equal to 
-                # argmin((X-Y)^2), it would be a waste of computation
-                sum_squares = tf.reduce_sum(tf.square(tf.subtract( rep_points, rep_centroids) ), 
-                                                reduction_indices = 2)
-
-                # Use argmin to select the lowest-distance point
-                # This gets a matrix of size N x 1
-                best_centroids = tf.argmin(sum_squares, axis = 1)
-
-                # This Sums vector X by the best_centroids indexes(assigned clusters)
-                # And returns a vector of size K x M
-                total_means_aux = tf.unsorted_segment_sum(X, best_centroids, K)
-
-                # This counts how many data points by best_centroids indexes(assigned clusters)
-                # And returns a vector of size K x M
-                count_means_aux = tf.unsorted_segment_sum(tf.ones_like(X), best_centroids, K)
-                
-                # Calculates the new Center for this GPU
-                # Returns a matrix of size K x M
-                means_with_nan = tf.div( total_means_aux, count_means_aux )
-
-                # Error treatment for when there are zeros in count_means_aux
-                means = tf.where(tf.is_nan(means_with_nan), tf.zeros_like(means_with_nan), means_with_nan);
-
-                # Cost Function, wihch would used for stopping criteria
-                #cost = tf.reduce_sum( tf.reduce_min(sum_squares, axis = 1) ) ## Commented for performance
-                #partial_cost.append(cost) ## Commented for performance
-                
-            with tf.device('/cpu:0'):
-                y_count = tf.bincount(tf.to_int32(best_centroids), maxlength = K, minlength = K)
-                y_count_float = tf.cast(y_count, dtype = tf.float64)
-                partial_mu =  tf.multiply( tf.transpose(means), y_count_float )
-
-                partial_directions.append( y_count_float )
-                partial_values.append( partial_mu )
-                
-    with tf.name_scope('global') :
-        with tf.device('/cpu:0') :
-            sum_direction = tf.add_n( partial_directions )
-            sum_mu = tf.add_n( partial_values )
-            #total_cost = tf.add_n( partial_cost )
-
-            rep_sum_direction = tf.reshape(tf.tile(sum_direction, [M]), [M, K])
-            new_centers = tf.transpose( tf.div(sum_mu, rep_sum_direction) )
-
-            update_centroid = tf.group( global_centroids.assign(new_centers) )
-        
-    setup_time = float( time.time() - setup_ts )
-    
-    initialization_ts = time.time()
-    
-    config = tf.ConfigProto( log_device_placement = True )
-    config.gpu_options.allow_growth = True
-    config.gpu_options.allocator_type = 'BFC'
-    sess = tf.Session( config = config )
-    
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    
-    initialization_time = float( time.time() - initialization_ts ) 
-    
-    computation_time = 0.0
-    for i in range(n_max_iters):
-        aux_ts = time.time()
-        [result, _] = sess.run([global_centroids, update_centroid])
-        computation_time += float(time.time() - aux_ts)
-    
-    end_resut = {   'end_center'          : result             ,
-                    'init_center'         : initial_centers    ,
-                    'setup_time'          : setup_time         ,
-                    'initialization_time' : initialization_time,
-                    'computation_time'    : computation_time   ,
-                    'n_iter'              : i+1
-                }
-
     return end_resut
 
 def distribuited_k_means(data_batch, K, GPU_names, initial_centers, n_max_iters):
@@ -369,7 +253,7 @@ def distribuited_k_means(data_batch, K, GPU_names, initial_centers, n_max_iters)
 
     with tf.Session( config = config ) as sess:
         initialization_ts = time.time()
-        sess.run(tf.global_variables_initializer(), feed_dict={all_data: data_X})
+        sess.run(tf.global_variables_initializer(), feed_dict={all_data: data_batch})
         initialization_time = float( time.time() - initialization_ts ) 
     
         computation_time = 0.0
@@ -388,25 +272,16 @@ def distribuited_k_means(data_batch, K, GPU_names, initial_centers, n_max_iters)
 
     return end_resut
 
-def make_data(n_obs, n_dim, seed):
-    (X, Y) = make_classification(n_samples            = n_obs    , 
-                                 n_features           = n_dim    ,
-                                 n_informative        = n_dim    ,
-                                 n_redundant          = 0        ,
-                                 n_classes            = 2        ,
-                                 n_clusters_per_class = 1        ,
-                                 shuffle              = True     ,
-                                 random_state         = seed      )
-    return (X, Y)
-
 def run_experiments(batches, GPU_names, K, initial_centers, n_max_iters, func):
     result = []
     setup_time = 0.0
     initialization_time = 0.0
     computation_time = 0.0
 
+    print(func)
+
     for batch in batches:
-        batch_run = func(data_batch, K, GPU_names, initial_centers, n_max_iters)
+        batch_run = func(batch, K, GPU_names, initial_centers, n_max_iters)
         result.append(batch_run['end_center'])
 
         setup_time += batch_run['setup_time']
@@ -433,6 +308,7 @@ def main(n_obs, n_dim, K, GPU_names, n_max_iters, seed , log_file, method_name, 
 
     # Defining max batch size based on previous tests
     data_size = n_dim * n_obs * X.dtype.itemsize
+    max_size = 1024 * 1024 * 1024
 
     if (len(GPU_names) == 8):
         max_size = 134217728 * 2 * X.dtype.itemsize
@@ -443,10 +319,11 @@ def main(n_obs, n_dim, K, GPU_names, n_max_iters, seed , log_file, method_name, 
     elif (len(GPU_names) == 2):
         max_size = 8388608 * 11 * X.dtype.itemsize
 
+    # Batching data
     num_batches = np.ceil(data_size / max_size)
-
     batches = np.array_split(X, num_batches)
 
+    # Running methods
     try:
         if method_name == 'distributedKMeans':
             run_result = run_experiments(batches            = batches, 
@@ -506,90 +383,92 @@ def main(n_obs, n_dim, K, GPU_names, n_max_iters, seed , log_file, method_name, 
                                   str( data_to_append['n_iter'] ) ])
 
         str_to_write = str(str_to_write) + '\n'
-        print('log_file =', log_file)
 
         with open(log_file, 'a') as f:
-            f.write(str_to_write)    
+            f.write(str_to_write)
+
+        print('log_file =', log_file)
 
     return return_status
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description = 'Compile Distribuited K Means Results.')
-  
-  parser.add_argument("--n_obs"                                             ,
-                      dest     = "n_obs"                                    ,
-                      required = True                                       ,
-                      metavar  = "int"                                      ,
-                      type     = lambda x: make_valid_int(parser, x)        ,
-                      help     = "Number of Observations for the Test !!!" )
+    parser = argparse.ArgumentParser(description = 'Compile Distribuited K Means Results.')
 
-  parser.add_argument("--n_dim"                                         ,
-                      dest     = "n_dim"                                ,
-                      required = True                                   ,
-                      metavar  = "int"                                  ,
-                      type     = lambda x: make_valid_int(parser, x)    ,
-                      help     = "Number of Dimensions for the Test !!!" )
+    parser.add_argument("--n_obs"                                             ,
+                        dest     = "n_obs"                                    ,
+                        required = True                                       ,
+                        metavar  = "int"                                      ,
+                        type     = lambda x: make_valid_int(parser, x)        ,
+                        help     = "Number of Observations for the Test !!!" )
 
-  parser.add_argument("--K"                                             ,
-                      dest     = "K"                                    ,
-                      required = True                                   ,
-                      metavar  = "int"                                  ,
-                      type     = lambda x: make_valid_int(parser, x)    ,
-                      help     = "Number of K Centers for the Test !!!" )
+    parser.add_argument("--n_dim"                                         ,
+                        dest     = "n_dim"                                ,
+                        required = True                                   ,
+                        metavar  = "int"                                  ,
+                        type     = lambda x: make_valid_int(parser, x)    ,
+                        help     = "Number of Dimensions for the Test !!!" )
 
-  parser.add_argument("--n_GPUs"                                                ,
-                      dest     = "GPU_names"                                    ,
-                      required = True                                           ,
-                      metavar  = "int"                                          ,
-                      type     = lambda x: parse_valid_gpus_names(parser, x)    ,
-                      help     = "Number of GPUs !!!" )
+    parser.add_argument("--K"                                             ,
+                        dest     = "K"                                    ,
+                        required = True                                   ,
+                        metavar  = "int"                                  ,
+                        type     = lambda x: make_valid_int(parser, x)    ,
+                        help     = "Number of K Centers for the Test !!!" )
 
-  parser.add_argument("--n_max_iters"                                           ,
-                      dest     = "n_max_iters"                                  ,
-                      required = True                                           ,
-                      metavar  = "int"                                          ,
-                      type     = lambda x: make_valid_int(parser, x)            ,
-                      help     = "Number of iterations before stopping!!!" )
+    parser.add_argument("--n_GPUs"                                                ,
+                        dest     = "GPU_names"                                    ,
+                        required = True                                           ,
+                        metavar  = "int"                                          ,
+                        type     = lambda x: parse_valid_gpus_names(parser, x)    ,
+                        help     = "Number of GPUs !!!" )
 
-  parser.add_argument("--seed"                                      ,
-                      dest     = "seed"                             ,
-                      required = True                               ,
-                      metavar  = "int"                              ,
-                      type     = lambda x: make_valid_int(parser, x),
-                      help     = "Seed Value !!!"                    )
+    parser.add_argument("--n_max_iters"                                           ,
+                        dest     = "n_max_iters"                                  ,
+                        required = True                                           ,
+                        metavar  = "int"                                          ,
+                        type     = lambda x: make_valid_int(parser, x)            ,
+                        help     = "Number of iterations before stopping!!!" )
 
-  parser.add_argument("--log_file"                                       ,
-                      dest     = "log_file"                              ,
-                      required = True                                    ,
-                      metavar  = "FILE"                                  ,
-                      type     = lambda x: is_valid_file(parser, x)      ,
-                      help     = "log_file Name, this would be a CSV !!!" )
+    parser.add_argument("--seed"                                      ,
+                        dest     = "seed"                             ,
+                        required = True                               ,
+                        metavar  = "int"                              ,
+                        type     = lambda x: make_valid_int(parser, x),
+                        help     = "Seed Value !!!"                    )
 
-  parser.add_argument("--method_name"                                                   ,
-                      dest     = "method_name"                                          ,
-                      required = True                                                   ,
-                      metavar  = "str"                                                  ,
-                      type     = lambda x: make_valid_method(parser, x)                 ,
-                      help     = "Method Name Can Be :" + 
-                                 "distribuitedKMeans or distribuitedFuzzyCMeans !!!" )
+    parser.add_argument("--log_file"                                       ,
+                        dest     = "log_file"                              ,
+                        required = True                                    ,
+                        metavar  = "FILE"                                  ,
+                        type     = lambda x: is_valid_file(parser, x)      ,
+                        help     = "log_file Name, this would be a CSV !!!" )
 
-  parser.add_argument("--data_file"                                                     ,
-                      dest     = "data_file"                                            ,
-                      required = True                                                   ,
-                      metavar  = "str"                                                  ,
-                      type     = lambda x: check_file_exists(parser, x)                 ,
-                      help     = "Unable to find data file !!!" )
+    parser.add_argument("--method_name"                                                   ,
+                        dest     = "method_name"                                          ,
+                        required = True                                                   ,
+                        metavar  = "str"                                                  ,
+                        type     = lambda x: make_valid_method(parser, x)                 ,
+                        help     = "Method Name Can Be :" + 
+                        "distribuitedKMeans or distribuitedFuzzyCMeans !!!" )
 
-  args = parser.parse_args()
+    parser.add_argument("--data_file"                                                     ,
+                        dest     = "data_file"                                            ,
+                        required = True                                                   ,
+                        metavar  = "str"                                                  ,
+                        type     = lambda x: check_file_exists(parser, x)                 ,
+                        help     = "Unable to find data file !!!" )
 
-  status = main(n_obs       = args.n_obs      ,
-                n_dim       = args.n_dim      ,
-                K           = args.K          ,
-                GPU_names   = args.GPU_names  ,
-                n_max_iters = args.n_max_iters,
-                seed        = args.seed       ,
-                log_file    = args.log_file   ,
-                method_name = args.method_name,
-                data_file   = args.data_file )
+    args = parser.parse_args()
 
-  sys.exit(status)
+    status = main(n_obs       = args.n_obs      ,
+                  n_dim       = args.n_dim      ,
+                  K           = args.K          ,
+                  GPU_names   = args.GPU_names  ,
+                  n_max_iters = args.n_max_iters,
+                  seed        = args.seed       ,
+                  log_file    = args.log_file   ,
+                  method_name = args.method_name,
+                  data_file   = args.data_file )
+
+
+    sys.exit(status)
